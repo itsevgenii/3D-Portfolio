b8abb2bad1c482a308f931e06a262b05f1c1e5a2
itsevgenii <smazanovich@icloud.com>
2025-11-18 17:03:34 +0000

    Adding project files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text 3xl underlinegit">Welcome to my 3d portfolio</h1>
+    </main>
+  );
+}
+
+export default App;
