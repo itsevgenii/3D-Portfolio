2ad8873d7aad3f026d0ad10d113afcddfc69d888
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-11-18 17:33:17 +0000

    added animated text on front page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Hero from "./sections/Hero";
 
 function App() {
   return (
-    <main>
-      <h1 className="text 3xl underlinegit">Welcome to my 3d portfolio</h1>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 }
 
