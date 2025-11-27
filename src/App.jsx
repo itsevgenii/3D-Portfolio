dc250fbb16a42508305cf21303efadc79b65c315
itsevgenii <smazanovich@icloud.com>
2025-11-27 11:56:17 +0000

    created tailwind config file to type class names easier

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
+import LogoSection from "./components/LogoSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection
     </>
   );
 }
---
+++ src/components/LogoSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogoSection = () => {
+  return (
+    <div className="md my-20 my-10 relative">LogoSection</div>
+  )
+}
+
+export default LogoSection
