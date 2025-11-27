60a3b5ba7b6dbba8c1602680a212ea4afbd5c4ed
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-11-27 18:57:33 +0000

    added experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 10, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Certificates" },
-  { value: 4, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "+", label: "Group Projects" },
+  { value: 15, suffix: "+", label: "Years of Experience" },
+  { value: 200, suffix: "+", label: "Satisfied Clients" },
+  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
 const logoIconsList = [
