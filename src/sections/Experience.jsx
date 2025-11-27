7aa3a9867dfa9d4db6ccde65ec459971ebfc837e
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-11-27 18:31:53 +0000

    layout changed and fixed

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
   return (
     <section
       id="experience"
-      className="w-full md:mt-40 mt-20 section-padding xl:px-0"
+      className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
     >
       {" "}
       <div className="w-full h-full md:px-20 px-5">
@@ -16,26 +16,33 @@ const Experience = () => {
           sub=" My Career Overview"
         />
         <div className="mt-32 relative">
-          <div className="relative z-50 xl:space-y-32 space-y-10" />
-          {expCards.map((card, i) => (
-            <div key={i} className="exp-card-wrapper">
-              <div className="xl:w-2/6">
-                <GlowCard card={card} index={i}>
-                  <div>
-                    <img src={card.imgPath} alt={card.title} />
-                  </div>
-                </GlowCard>
-              </div>
-              <div className="xl:w-4/6">
-                <div className="flex items-start">
-                  <div className="timeline-wrapper">
-                    <div className="timeline" />
-                    <div className="gradient-line w-1 h-full" />
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((card, i) => (
+              <div key={i} className="exp-card-wrapper">
+                <div className="xl:w-2/6">
+                  <GlowCard card={card} index={i}>
+                    <div>
+                      <img src={card.imgPath} alt={card.title} />
+                    </div>
+                  </GlowCard>
+                </div>
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
