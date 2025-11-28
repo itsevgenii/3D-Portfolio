5751efd495581707ed2f55e90385824fb7e481b6
itsevgenii <smazanovich@icloud.com>
2025-11-28 17:22:34 +0000

    added tech stack section with 3d tech stack icons

---
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, Float } from "@react-three/drei";
+
+const TechIcon = ({ model }) => {
+  const scene = useGLTF(model.modelPath);
+
+  useEffect(() => {
+    if (model.name === "Interactive Developer") {
+      scene.scene.traverse((child) => {
+        if (child.isMesh && child.name === "Object_5") {
+          child.material = new THREE.MeshStandardMaterial({ color: "white" });
+        }
+      });
+    }
+  });
+
+  return (
+    <Canvas>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <spotLight
+        position={[10, 15, 10]}
+        angle={0.3}
+        penumbra={1}
+        intensity={2}
+      />
+      <Environment preset="city" />
+      <OrbitControls enableZoom={false} />
+      <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
+        <group scale={model.scale} rotation={model.rotation}>
+          <primitive object={scene.scene} />
+        </group>
+      </Float>
+    </Canvas>
+  );
+};
+
+export default TechIcon;
