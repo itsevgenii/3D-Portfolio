79daa7bafd377269da4bd854b24fba43c706bfc6
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-11-29 11:19:55 +0000

    email js contact me setion done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import LogoSection from "./sections/LogoSection";
 import FeatureCards from "./sections/FeatureCards";
 import Experience from "./sections/Experience";
 import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
       <FeatureCards />
       <Experience />
       <TechStack />
+      <Testimonials />
+      <Contact />
     </>
   );
 }
